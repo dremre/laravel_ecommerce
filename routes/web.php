813ec04764e73b0b14afe4220307a0aed96cb212
alkan <alkan4802@gmail.com>
2021-04-05 23:49:23 +0300

    2. yukleme

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Emre',function (){
+   return "Emre Alkan";
+});
+
+Route::get('api/v1/merhaba',function (){
+   return ['mesaj'=>'Emre Alkan:)'];
+});
+
+Route::get('/urun/{urunadi}/{id?}',function ($urunadi,$id=0){
+    return "Ürün adi: $urunadi /
+            Ürün id : $id";
+})->name('urun-detay');
+
+Route::get('/kampanya',function (){
+   return redirect()->route('urun-detay',['urunadi'=>'elma','id'=>5]);
+});
